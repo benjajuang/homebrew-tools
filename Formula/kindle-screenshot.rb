9a455304a7abbc9f9c4f6e3999fede6ae9539d6a
benjajuang <benq11607@gmail.com>
2025-08-01 13:41:18 +0800

    kindle-screenshot 1.0.1: update URL & sha256

--- Formula/kindle-screenshot.rb
+++ Formula/kindle-screenshot.rb
@@ -1,8 +1,8 @@
 class KindleScreenshot < Formula
   desc "Grab screenshots of Kindle windows on macOS"
   homepage "https://github.com/benjajuang/kindle-screenshot"
-  url "https://github.com/benjajuang/kindle-screenshot/archive/v1.0.0.tar.gz"
-  sha256 "ebaa8e6713596dfc182ad05701bccb598ef7e4bfcf6668f9cdc86b6e4907d560"
+  url "https://github.com/benjajuang/kindle-screenshot/archive/v1.0.1.tar.gz"
+  sha256 "121e9d20052835e5d96f43a30ebf8e07eef7a5b23eb993cc48b221b0c551010f"
 
   def install
     bin.install "screenshot.command" => "ksnap"
