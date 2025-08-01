37b5f9ae2a5026a8fd362157acaed6dea677e344
benjajuang <benq11607@gmail.com>
2025-08-02 02:23:28 +0800

    chnsub 1.0.2: bump URL & sha256

--- Formula/chnsub.rb
+++ Formula/chnsub.rb
@@ -1,34 +1,60 @@
 class Chnsub < Formula
+  include Language::Python::Virtualenv
+
   desc "Generate smart Chinese subtitles (SRT files)"
   homepage "https://github.com/benjajuang/chnsub"
-  url "https://github.com/benjajuang/chnsub/archive/v1.0.0.tar.gz"
-  sha256 "39d342ca90621be8b7083acd2d55f996b1bd28dbcfe1b3c62792ab03b47c7200"
+  url "https://github.com/benjajuang/chnsub/archive/v1.0.2.tar.gz"
+  sha256 "5344ff4f0f427f46575c1a7da78664545e94d74fbdba1e2d29b93a73da7773b3"
 
   depends_on "python@3.11"
 
+  resource "yt-dlp" do
+    url "https://files.pythonhosted.org/packages/7e/3a/343f7a0024ddd4c30f150e8d8f57fd7b924846f97d99fc0dcd75ea8d2773/yt_dlp-2025.7.21.tar.gz"
+    sha256 "46fbb53eab1afbe184c45b4c17e9a6eba614be680e4c09de58b782629d0d7f43"
+  end
+
+  resource "openai" do
+    url "https://files.pythonhosted.org/packages/d8/9d/52eadb15c92802711d6b6cf00df3a6d0d18b588f4c5ba5ff210c6419fc03/openai-1.98.0.tar.gz"
+    sha256 "3ee0fcc50ae95267fd22bd1ad095ba5402098f3df2162592e68109999f685427"
+  end
+
+  resource "pysubs2" do
+    url "https://files.pythonhosted.org/packages/31/4a/becf78d9d3df56e6c4a9c50b83794e5436b6c5ab6dd8a3f934e94c89338c/pysubs2-1.8.0.tar.gz"
+    sha256 "3397bb58a4a15b1325ba2ae3fd4d7c214e2c0ddb9f33190d6280d783bb433b20"
+  end
+
+  resource "python-dotenv" do
+    url "https://files.pythonhosted.org/packages/source/p/python-dotenv/python_dotenv-1.1.1.tar.gz"
+    sha256 "a8a6399716257f45be6a007360200409fce5cda2661e3dec71d23dc15f6189ab"
+  end
+
+  resource "whisperx" do
+    url "https://files.pythonhosted.org/packages/4e/97/38238c00ca5bdaaa3dfb3046179a6757ca2400643e746e605cd11941d451/whisperx-3.4.2.tar.gz"
+    sha256 "5f07ec56b129ec5da3600459a645b6676c711e2e4f58f3e094617cabcc032a9b"
+  end
+
+  resource "transformers" do
+    url "https://files.pythonhosted.org/packages/source/t/transformers/transformers-4.54.1.tar.gz"
+    sha256 "b2551bb97903f13bd90c9467d0a144d41ca4d142defc044a99502bb77c5c1052"
+  end
+
   def install
-    libexec.install Dir["*"]
-    # create a wrapper `chnsub` in bin/ that invokes your main script
-    bin.write_exec_script "#{libexec}/main.py"
+    virtualenv_install_with_resources
   end
 
   def caveats
     <<~EOS
-      chnsub has been installed, but you must still install its Python dependencies:
-        pip3 install --upgrade -r #{opt_libexec}/requirements.txt
+      torch isn’t vendored (wheels-only); after:
+        brew install benjajuang/tools/chnsub
+      you must then run once:
+        pip3 install torch
 
-      If you prefer, you can use a virtualenv:
-        python3 -m venv ~/.venvs/chnsub
-        source ~/.venvs/chnsub/bin/activate
-        pip install --upgrade -r #{opt_libexec}/requirements.txt
-
-      Then just run:
+      Then you can simply:
         chnsub --help
     EOS
   end
 
   test do
-    # this will at least verify the wrapper exists
-    assert_predicate bin/"chnsub", :exist?
+    assert_match "Generate smart Chinese subtitles", shell_output("#{bin}/chnsub --help")
   end
 end
